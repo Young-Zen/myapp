d4a376ae1f3c40d37cc6235162a14aafac66838b
Young-Zen <40934357+Young-Zen@users.noreply.github.com>
2021-02-18 00:13:38 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+    "time"
+)
+
+func main()  {
+    http.HandleFunc("/", HelloWorldHandler)
+    err := http.ListenAndServe("0.0.0.0:80", nil)
+    if err != nil {
+        fmt.Println("服务器错误")
+    }
+}
+
+
+func HelloWorldHandler(res http.ResponseWriter, req *http.Request)  {
+    fmt.Printf("[%s] %s %s\n", time.Now().Format("2006-01-02 15:04:05"), req.Method, req.URL)
+	  hostname, _ := os.Hostname()
+	  fmt.Fprintf(res, "Version: v1, Hostname: %s\n", hostname)
+}
